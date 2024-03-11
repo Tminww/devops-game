8d1f9bdf6c363c854163b996530105e384c7c6fe
Tminww <you@example.com>
2024-03-11 18:26:25 +0300

    Add: logger for sqlalchemy

--- backend/src/database/setup.py
+++ backend/src/database/setup.py
@@ -2,6 +2,20 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.config import settings
 
+import logging
+
+logging.basicConfig(
+    filename="./src/log/sqlalchemy.log",
+    filemode="a",
+    format="[%(asctime)s] [%(name)s] [%(levelname)s] > %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+logging.getLogger("sqlalchemy.pool").setLevel(logging.DEBUG)
+# logging.getLogger("sqlalchemy.orm").setLevel(logging.INFO)
+
+
 async_engine = create_async_engine(
     url=settings.DATABASE_URL,
     echo=True,
