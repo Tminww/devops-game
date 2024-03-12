753af5f0b27547ab68ec5463f478dfb3dd15be70
Tminww <you@example.com>
2024-03-12 19:15:34 +0300

    Add: new logic for router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import Result from "./components/Result.vue";
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <Result msg="Заливка" />
+      <!-- <img
+        alt="logo"
+        class="logo"
+        src="@/assets/logo.jpg"
+        width="125"
+        height="125"
+      /> -->
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Смотреть результаты</RouterLink>
+        <RouterLink to="/field">Играть</RouterLink>
       </nav>
     </div>
   </header>
